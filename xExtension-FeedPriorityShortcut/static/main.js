6e8eaaf1367ccb29dfd958090a97b4c90bd9d24c
Axel Leroy <axeleroy@users.noreply.github.com>
2025-10-16 13:31:34 +0200

    Modify FreePriorityShortcut to toggle between main stream and category

--- xExtension-FeedPriorityShortcut/static/main.js
+++ xExtension-FeedPriorityShortcut/static/main.js
@@ -17,36 +17,30 @@ window.addEventListener('load', () => {
         const i = li.dataset.feedId
         const p = context.extensions.FeedPriorityShortcut.priority[i]
         const t = context.extensions.FeedPriorityShortcut.tooltips
-        const d = document.createElement('div')
-
-        d.classList.add('feed-priority-shortcut', 'dropdown')
-        d.innerHTML =
-            `<div id="dropdown-feed-${i}" class="dropdown-target"></div>
-            <a class="dropdown-toggle" href="#dropdown-feed-${i}">${p}</a>
-            <ul class="dropdown-menu">
-                <li class="item" title="${t.important}">📌</li>
-                <li class="item" title="${t.main_stream}">🏠</li>
-                <li class="item" title="${t.category}">📁</li>
-                <li class="item" title="${t.archived}">🔒</li>
-            </ul>
-            <a class="dropdown-close" href="#close">❌</a>`
-
-        li.appendChild(d)
-    })
-
-    // Add listeners
-    document.querySelectorAll('.feed-priority-shortcut .item').forEach((li) => {
-        li.addEventListener('click', (e) => {
+	const d = document.createElement('button');
+        
+	d.title = 'Toggle priority';
+        d.classList.add('feed-priority-shortcut');
+        d.innerHTML = p;
+        d.addEventListener('click', (e) => {
+            const currentPriority = e.target.textContent;
+            let priority;
+            if (currentPriority === '🏠') {
+                priority = '📁'
+            } else {
+                priority = '🏠'
+            }
             fetch(context.extensions.FeedPriorityShortcut.postUrl, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
                     _csrf: context.csrf,
                     feed_id: e.target.closest('.feed.item').dataset.feedId,
-                    priority: e.target.textContent
+                    priority
                 })
             })
             .then(() => location.reload())
-        })
+        });
+        li.appendChild(d)
     })
 })
